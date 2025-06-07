fe68e8fc21e1de8596094eab65498b55ac32a8c4
Hasnaa mohamed <hasnaamo1234@gmail.com>
2025-06-07 15:04:12 +0300

    Clothing_product.java

--- Clothing_product.java
+++ Clothing_product.java
@@ -1,21 +1,29 @@
 
 package ecommerce_system;
-import javax.swing.*;
-import java .awt.*;
-import java .awt.Graphics ;
-public class Gui2  extends JPanel{
-   @Override
-   protected void paintComponent(Graphics g){
-   super.paintComponent(g);
-   g.setColor(Color.CYAN.darker());
-   g.fill3DRect(0,0, 1620, 200, true);
-   
-   g.fillOval(0, 0, 1300, 260);
-  
-   g.setColor(Color.YELLOW);
-   g.fillOval(40, 50,90, 50);
-   
-   g.setColor(Color.BLACK.brighter());
-   g.fill3DRect(490, 253, 300, 350, true);
-   
-}}
+
+public class Clothing_product extends Product {
+   String size;
+    String fabric; 
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFabric() {
+        return fabric;
+    }
+    
+    public void setFabric(String fabric) {
+        this.fabric = fabric;
+    }
+    public Clothing_product(){}
+    public Clothing_product( int product_id,String name,float price,String size,String fabric){ 
+    super  ( product_id,name, price);  
+        this.size=size;
+    this.fabric=fabric;}
+    
+}
